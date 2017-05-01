a2ac20937e46c6e96f84a7854e151146c41324a4
Benjamin Wilburn <bwilburnstrength@gmail.com>
2017-05-01 17:23:57 -0500

    changed query from just grabbing program to grabbing all workouts that belong to that program and then all exercise_groups/exercise blocks and passing them to the template

--- FITist_project/user_profiles/views.py
+++ FITist_project/user_profiles/views.py
@@ -7,6 +7,9 @@ from user_profiles.forms import UserForm
 from django.contrib.auth.models import User
 from user_profiles.models import create_user_profile
 from workout_programs.models import get_program_instance
+from workout_programs.models import ExerciseBlock
+from workout_programs.models import ExerciseGroup
+from workout_programs.models import Workout
 
 
 def create_new_user(request):
@@ -46,16 +49,36 @@ def get_profile(request, username=None):
         return redirect('profiles:user_profile', username=curr_user.username)
 
 
+@login_required(login_url='/user_profiles/login/')
 def view_program(request, username=None):
     curr_user = request.user
     if not username:
         return redirect('profiles:user_profile', username=curr_user.username)
     user = get_object_or_404(User, username=username)
+    workouts = Workout.objects.filter(program=user.profile.program_selected)
+    exercise_groups = list()
+    exercise_blocks = list()
+    all_groups = ExerciseGroup.objects.all()
+    all_blocks = ExerciseBlock.objects.all()
+    for group in all_groups:
+        if group.workout_in.program == user.profile.program_selected:
+            exercise_groups.append(group)
+    for block in all_blocks:
+        if block.assigned_group in exercise_groups:
+            exercise_blocks.append(block)
+    print('WORKOUTS', workouts)
+    print('EXERCISE_GROUPS', exercise_groups)
+    print('EXERCISE_BLOCKS', exercise_blocks)
     if curr_user == user:
         return render(
             request,
             'user_profiles/program.html',
-            {'program': user.profile.program_selected}
+            {
+                'program': user.profile.program_selected,
+                'workouts': workouts,
+                'exercise_groups': exercise_groups,
+                'exercise_blocks': exercise_blocks
+            }
         )
     else:
         return redirect('profiles:user_profile', username=curr_user.username)
